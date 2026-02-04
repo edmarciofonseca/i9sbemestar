8d811b2c9fc88747150093a492b7162a903ccb47
Tiago Carneiro <tiago@phoenixsoftware.ca>
2026-02-03 22:19:25 -0300

    Serviços modal detalhes

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 import './router.js';
+
+function abrirDialog(idDialog) {
+
+    const dialog = document.getElementById(idDialog);
+
+    if (!dialog)
+        return;
+
+    if (typeof dialog.showModal === "function") {
+        dialog.showModal();
+    }
+}
+
+function fecharDialog(idDialog) {
+
+    const dialog = document.getElementById(idDialog);
+
+    if (!dialog)
+        return;
+
+    dialog.close();
+}
+
+// 👇 torne globais para o HTML
+window.abrirDialog = abrirDialog;
+window.fecharDialog = fecharDialog;
